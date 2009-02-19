9d9992f1209d61b46872b75c3d2c1a4cc37afb13
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-02-19 19:29:24 +0000

    Se agrega el metodo toUnderscore.

--- YuppPHPFramework/core/basic/core.basic.String.class.php
+++ YuppPHPFramework/core/basic/core.basic.String.class.php
@@ -27,5 +27,39 @@ class String {
       }
       return lcfirst($str);
    }
+   
+   function toUnderscore($string)
+   {
+     //return preg_replace("/[A-Z ]/", "/[a-z_]/", $string);
+    return $addr = strtr($string,
+                         "ABCDEFGHIJKLMNOPQRSTUVWXYZ ", 
+                         "abcdefghikklmnopqrstuvwxyz_");
+     /*
+     $pattern[0] = '/\&/';
+     $pattern[1] = '/</';
+     $pattern[2] = "/>/";
+     $pattern[3] = '/\n/';
+     $pattern[4] = '/"/';
+     $pattern[5] = "/'/";
+     $pattern[6] = "/%/";
+     $pattern[7] = '/\(/';
+     $pattern[8] = '/\)/';
+     $pattern[9] = '/\+/';
+     $pattern[10] = '/-/';
+     $replacement[0] = '&amp;';
+     $replacement[1] = '&lt;';
+     $replacement[2] = '&gt;';
+     $replacement[3] = '<br>';
+     $replacement[4] = '&quot;';
+     $replacement[5] = '&#39;';
+     $replacement[6] = '&#37;';
+     $replacement[7] = '&#40;';
+     $replacement[8] = '&#41;';
+     $replacement[9] = '&#43;';
+     $replacement[10] = '&#45;';
+     return preg_replace($pattern, $replacement, $string);
+     */
+   }
+   
 }
 ?>
