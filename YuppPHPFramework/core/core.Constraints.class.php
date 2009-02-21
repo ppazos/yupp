2aa0da8136fc037a992a0868570f93259ad2b59b
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-02-21 17:06:01 +0000

    Correcciones para hacer a Yupp compatible con PHP 5.2.0

--- YuppPHPFramework/core/core.Constraints.class.php
+++ YuppPHPFramework/core/core.Constraints.class.php
@@ -154,11 +154,11 @@ class Between extends Constraint {
 
 class EmailConstraint extends Matches {
  
-   private $email_pattern = "/^[a-z]+[a-z0-9]*[\.|\-|_]?[a-z0-9]+@([a-z]+[a-z0-9]*[\.|\-]?[a-z]+[a-z0-9]*[a-z0-9]+){1,4}\.[a-z]{2,4}$/";
+   const email_pattern = '/^[a-z]+[a-z0-9]*[\.|\-|_]?[a-z0-9]+@([a-z]+[a-z0-9]*[\.|\-]?[a-z]+[a-z0-9]*[a-z0-9]+){1,4}\.[a-z]{2,4}$/';
    
    public function __construct()
    {
-      parent::__construct($this->email_pattern);
+      parent::__construct( self::email_pattern );
    }
 }
 
