d34c8ed2c2298f10dfb7bb8e82498a57e80e51f7
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-02-28 17:24:51 +0000

    Yupp PHP Framework v0.1.6.2
    - Correccion en PersistentManager.generate() para que genere todas las tablas intermedias con la columna "ord" que se utiliza cuando el atributo hasMany es de tipo LIST, cuando es de tipo SET o COLLECTION, no se toma en cuenta.
    
    - Se agregó soporte para incluir CSS o imagenes desde un componente. Para esto se modificó el helper "css" y se agregó el helper "img".
    
    - Corrección al helper "errors", lanzaba un error si el elemento no tenía errores.
    - Correcciön al método "hasErrors" de PersistentObject, que no verificaba que el campo errors fuera NULL.
    
    - Se agrega el constructor para la condicion Not Equal a la clase Condition.
    
    - Correccion del metodo addOrder de la clase Criteria2.
    
    - Hay un problema con la extraccion de los parametro de la url>
    
    http://localhost:8081/YuppPHPFramework/portal/page/display/mi_pagina_bbb/sdfda/asdf?as=sdfg
    
    El tema fue que al intriducir el tema del ruteo, UlrProcessing, quien antes hacia el trabajo de crear los params de la url, no se llama mas, y ahora el ruteo solo toma params de POST y GET y no considera la url.
    
    - Varias correcciones para compatibilizar Yupp con versiones anteriores de PHP (PHP 5.2.0 y 5.2.1).
    
    - Se agregó el método firstToUpper a core.basic.String.
    
    - Se agregaron los siguientes campos a la configuración de Yupp (clase YuppConfig):
      - currentMode: indica el modo de ejecucion de la aplicacion (development, production o testing).
      - modeDefaultMapping: indica que accion debe ejecutarse por defecto, dependiendo del modo actual, al acceder a la aplicación.
    
    - Se agregó el método addCustomParams en la clase routing.Filter

---
+++ tags/v0162/core/mvc/webflow/core.mvc.webflow.CurrentFlows.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Almacena los web flows que estan en ejecucion en este momento. Espersistente entre requests.
+ * Pregunta: es posible que un usuario tenga mas de un flow ejecutandose en el mismo momento?
+ * ESTO PASARIA SOLO SI EL FLOW TIENE SUBFLOWS, PERO SI SALE DEL FLOW, ESTE TENDRIA QUE ELIMINARSE, O NO?.
+ * Tal vez el controller deberia guardar el flow en ejecucion y manejarlo el (y administrar la key).
+ */
+class CurrentFlows {
+
+   private $flows = array();
+
+   public static function &getInstance()
+   {
+      $instance = NULL;
+      if ( !YuppSession::contains("_current_flows_singleton_instance") )
+      {
+         $instance = new CurrentFlows();
+         YuppSession::set("_current_flows_singleton_instance", $instance);
+      }
+      else
+      {
+         $instance = YuppSession::get("_current_flows_singleton_instance");
+      }
+
+      return $instance;
+   }
+   
+   public function update( WebFlow &$flow )
+   {
+      $this->addFlow( &$flow ); // Para hacer update es necesario agregarlo de nuevo :S de otra forma no funciona.
+   }
+
+   private function __construct()
+   {
+   }
+
+   public function addFlow( WebFlow &$flow )
+   {
+      $this->flows[ $flow->getId() ] = $flow;
+      YuppSession::set("_current_flows_singleton_instance", $this); // actualizo la variable en la session...
+   }
+   
+   public function &getFlow( $flowId )
+   {
+   	return $this->flows[ $flowId ];
+   }
+
+}
+
+?>
---
+++ tags/v0162/model/model.C1hasMany.class.php
@@ -0,0 +1,41 @@
+<?php
+class C1hasMany extends PersistentObject
+{
+	function __construct($args = array (), $isSimpleInstance = false)
+	{
+	   $this->addAttribute("nombre", Datatypes :: TEXT);
+
+		parent :: __construct($args, $isSimpleInstance);
+	}
+
+	public static function listAll($params)
+	{
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: listAll($params);
+	}
+
+	public static function count()
+	{
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: count();
+	}
+
+	public static function get($id)
+	{
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: get($id);
+	}
+
+	public static function findBy(Condition $condition, $params)
+	{
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: findBy($condition, $params);
+	}
+
+	public static function countBy(Condition $condition)
+	{
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: countBy($condition);
+	}
+}
+?>
