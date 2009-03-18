7ac141bcd2a0ff38bc62902fda10406c46887828
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-03-18 04:18:21 +0000

    Se agrega operacion para filtrar caracteres raros.

--- YuppPHPFramework/core/basic/core.basic.String.class.php
+++ YuppPHPFramework/core/basic/core.basic.String.class.php
@@ -2,9 +2,6 @@
 
 class String {
 
-   function String() {
-   }
-    
    public static function endsWith($str, $suffix)
    {
       //echo "'" . substr( $str, -strlen($suffix), strlen($str) ) . "'";
@@ -27,7 +24,7 @@ class String {
       }
       return lcfirst($str);
    }
-   
+
    public static function firstToUpper( $str )
    {
       if(false === function_exists('ucfirst')) // Podria no existir la funcion, segun la doc.
@@ -37,12 +34,27 @@ class String {
       return ucfirst($str);
    }
    
+   /**
+    * Quita los caracteres que tengan tildes o las enies o dieresis, 
+    * transformandolas en las mismas letras sin tildes o enies.
+    */
+   function filterCharacters($string)
+   {
+      // Si se ve raro es porque el visor esta mal configurado en su enconding type, este string tiene
+      // primero las vocales acentuadas, luego las vocales con dieresis y por ultimo la enie.
+      // OTRA FORMA DE ESPECIFICAR LOS CARACTERES EN ES NOTACION OCTAL. (CUIDADO, SI SE EDITA y SALVA PUEDE GUARDAR MAL ESTOS CARACTERES).
+      // http://weblogtoolscollection.com/b2-img/convertaccents.phps
+      return strtr($string,
+                   "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ",
+                   "AAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy");
+   }
+   
    function toUnderscore($string)
    {
      //return preg_replace("/[A-Z ]/", "/[a-z_]/", $string);
-    return $addr = strtr($string,
-                         "ABCDEFGHIJKLMNOPQRSTUVWXYZ ", 
-                         "abcdefghikklmnopqrstuvwxyz_");
+    return strtr($string,
+                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ ", 
+                 "abcdefghikklmnopqrstuvwxyz_");
      /*
      $pattern[0] = '/\&/';
      $pattern[1] = '/</';
@@ -69,6 +81,6 @@ class String {
      return preg_replace($pattern, $replacement, $string);
      */
    }
-   
+
 }
 ?>
