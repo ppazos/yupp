92e507e4b29af6a8f03f3bf78caba6d3d95ba06e
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-05-02 16:52:16 +0000

    Correcciones al web flow.

--- YuppPHPFramework/core/routing/core.routing.Executer.class.php
+++ YuppPHPFramework/core/routing/core.routing.Executer.class.php
@@ -60,107 +60,112 @@ class Executer {
            }
    
            // Debe verificar si tiene la accion y si la puede ejecutar, si no va a index.
-           
            $controllerInstance = new $controllerClassName($controller, $action, $this->params); // Se usa abajo!!!
    
-// ================================================================================================
-// Quiero arrojar esta except y que la agarre el try del index.php
-//           try
-//           {
-              if ( $controllerInstance->flowExists($action) ) // Si es un web flow
+
+           // Si hay except la agarra en el try del index.php
+
+           if ( $controllerInstance->flowExists($action) ) // Si es un web flow
+           {
+              //Logger::show("ES FLOW " . __FILE__ . " " . __LINE__);
+            
+              if (!$controllerInstance->flowLoaded($action))
               {
-                 //Logger::show("ES FLOW");
+                //Logger::show("== FLOW NOT LOADED ==");
+              	 $controllerInstance->loadFlow($action);
+              }
+            
+              // ===============================================================================
+              // Get Flow from controller
+           	  $flow = $controllerInstance->getFlow($action); // La accion es el nombre del flow.
+              
+              if (!$flow->isInitialized())
+              {
+              	  $flow->init();
+              }
+              
+              // ===============================================================================
+              // Execute Controller Flow Action
+              $flowActionName = $flow->getCurrentState()->getName() . "Action";
+              $flowExecutionResult = $controllerInstance->{$flowActionName}( &$flow ); // Esta es ya la accion de pasar al otro estado.??? deberia ser en el move...
+        
+              Logger::show( "Salida del flow: $flowExecutionResult, " . __FILE__ . " " . __LINE__ );
+        
+              // $flowExecutionResult puede ser "move", NULL o un codigo de error.
+              if ( $flowExecutionResult === NULL )
+              {
+                 Logger::show( "Flow Execution Result = NULL, " . __FILE__ . " " . __LINE__ );
                
-                 if (!$controllerInstance->flowLoaded($action))
-                 {
-                   //Logger::show("== FLOW NOT LOADED ==");
-                 	 $controllerInstance->loadFlow($action);
-                 }
+              	  // TODO: debe mostrar la vista llamada: $currentState
+                 $params = array_merge($flow->getModel(), $this->params);
+                 //$model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params ); // En los params no se que pasarle, deberian ser el modelo del flow mas los params submiteados en el request anterior.
+                 $model_or_command = $controllerInstance->render( $flow->getCurrentState()->getName(), &$params ); // En los params no se que pasarle, deberian ser el modelo del flow mas los params submiteados en el request anterior.
+              }
+              else if ( $flowExecutionResult === "move" )
+              {
+              	  // TODO: pasar al siguiente estado, debe ejecutar la accion de pasar de estado o directamente ejecuta la siguiente accion?
+                 Logger::show( "Flow Execution Result = MOVE, " . __FILE__ . " " . __LINE__ );
+              
+                 $eventName = $this->params["event"];
+                 
+                 //Logger::show( "EVENT: $eventName, " . __FILE__ . " " . __LINE__ );
                
-                 // ===============================================================================
-                 // Get Flow from controller
-              	  $flow = $controllerInstance->getFlow($action); // La accion es el nombre del flow.
-                 
-                 if (!$flow->isInitialized())
-                 {
-                 	  $flow->init();
-                 }
-                 
-                 // ===============================================================================
-                 // Execute Controller Flow Action
-                 $flowActionName = $flow->getCurrentState()->getName() . "Action";
-                 $flowExecutionResult = $controllerInstance->{$flowActionName}( &$flow ); // Esta es ya la accion de pasar al otro estado.??? deberia ser en el move...
-           
-                 // $flowExecutionResult puede ser "move", NULL o un codigo de error.
-                 if ( $flowExecutionResult === NULL )
-                 {
-                 	  // TODO: debe mostrar la vista llamada: $currentState
-                    $params = array_merge($flow->getModel(), $this->params);
-                    $model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params ); // En los params no se que pasarle, deberian ser el modelo del flow mas los params submiteados en el request anterior.
-                 }
-                 else if ( $flowExecutionResult === "move" )
-                 {
-                 	  // TODO: pasar al siguiente estado, debe ejecutar la accion de pasar de estado o directamente ejecuta la siguiente accion?
-                 
-                    $eventName = $this->params["event"];
-                    
-                    //Logger::show( "EVENT: $eventName" );
-                  
-                    // ==============================================================================================
-                    // FLOW MOVE!
-                    $flow->move( $eventName ); // Cambia el estado si puede, si no, tira una except (no existe la transicion! => la maquina esta mal definida).
+                 // ==============================================================================================
+                 // FLOW MOVE!
+                 $flow->move( $eventName ); // Cambia el estado si puede, si no, tira una except (no existe la transicion! => la maquina esta mal definida).
 
-                    if ($flow->getCurrentState()->isEndState())
-                    {
-                       // Ejecutar accion del ultimo estado
-                       $flowActionName = $flow->getCurrentState()->getName() . "Action";
-                       
-                       Logger::show("Estado final: " . $flow->getCurrentState()->getName());
-                       
-                       $controllerInstance->{$flowActionName}( &$flow ); // FIXME: podria retornar error y deberia volver al estado anterior y mostrar esa vista.
-           
-                       $params = array_merge($flow->getModel(), $this->params); // La accion puede haber agregado modelo (al flow o params del controller).
-                       
-                       // Quiero mostrar la vista correspondiente al nuevo estado. (antes de ejecutar init q cambia el estado!)
-                       $model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
+                 if ($flow->getCurrentState()->isEndState())
+                 {
+                    // Ejecutar accion del ultimo estado
+                    $flowActionName = $flow->getCurrentState()->getName() . "Action";
                     
+                    //Logger::show("END STATE: " . $flow->getCurrentState()->getName() . ", " . __FILE__ . " " . __LINE__ );
+                    
+                    $controllerInstance->{$flowActionName}( &$flow ); // FIXME: podria retornar error y deberia volver al estado anterior y mostrar esa vista.
+        
+                    $params = array_merge($flow->getModel(), $this->params); // La accion puede haber agregado modelo (al flow o params del controller).
+                    
+                    // Quiero mostrar la vista correspondiente al nuevo estado. (antes de ejecutar init q cambia el estado!)
+                    //$model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
+                    $model_or_command = $controllerInstance->render( $flow->getCurrentState()->getName(), &$params );
 
-                       // TODO: remover el flow del controller o inicializarlo de nuevo ya que se termino el que venia ejecutando.
-                       $flow->init();
-                    }
-                    else
-                    {
-                       // Quiero mostrar la vista correspondiente al nuevo estado.
-                       $model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
-                    	  $params = array_merge($flow->getModel(), $this->params);
-                    }
+                    // TODO: remover el flow del controller o inicializarlo de nuevo ya que se termino el que venia ejecutando.
+                    $flow->init();
                  }
                  else
                  {
-                 	  // TODO: tira un error, volver a la pagina actual y mostrar el error.
-                    $controllerInstance->addToFlash("message", $flowExecutionResult); // Pongo el error en flash.message
+                    //Logger::show( "NO ES END STATE: " . $flow->getCurrentState()->getName() . ", " . __FILE__ . " " . __LINE__ );
                     
                     // Quiero mostrar la vista correspondiente al nuevo estado.
-                    $params = array_merge($flow->getModel(), $this->params);
-                    $model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
+                    //$model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
+                    $model_or_command = $controllerInstance->render( $flow->getCurrentState()->getName(), &$params );
+                 	  $params = array_merge($flow->getModel(), $this->params);
                  }
               }
-              else // Es una accion comun.
+              else
               {
-                 //Logger::show("ES ACCION COMUN");
-                 $model_or_command = $controllerInstance->{$action}();
+                 Logger::show( "Flow Execution Result NO es MOVE, " . __FILE__ . " " . __LINE__ );
+               
+              	  // TODO: tira un error, volver a la pagina actual y mostrar el error.
+                 $controllerInstance->addToFlash("message", $flowExecutionResult); // Pongo el error en flash.message
+                 
+                 // Quiero mostrar la vista correspondiente al nuevo estado.
+                 $params = array_merge($flow->getModel(), $this->params);
+                 //$model_or_command = $controllerInstance->render( $controller . "/" . $flow->getCurrentState()->getName(), &$params );
+                 $model_or_command = $controllerInstance->render( $flow->getCurrentState()->getName(), &$params );
               }
-//           }
-//           catch (Exception $e)
-//           {
-//              echo $e->getMessage() . "<br/>";
-//              echo $e->getTraceAsString();
-//              exit();
-//           }
+           }
+           else // Es una accion comun.
+           {
+              //Logger::show("ES ACCION COMUN, " . __FILE__ . " " . __LINE__ );
+              $model_or_command = $controllerInstance->{$action}();
+           }
 
            // ======================================================================================
            // PROCESA COMANDO (resultado de before_filters o de ejecucion del controlador)
            // ======================================================================================
+           
+           //Logger::struct( $model_or_command, "MODEL OR COMMAND, " . __FILE__ . " " . __LINE__ );
            
            // Puede haber retornado solo modelo, un comando o nada.
            if ( is_array($model_or_command) ) // Es solo modelo
@@ -214,11 +219,6 @@ class Executer {
         
         } // Paso los before filters y ejecuto accion del controller de forma normal
         
-        // OK, tiene flash...
-//        echo "<pre>";
-//        print_r( $command );
-//        echo "</pre>";
-
         return $command;
     }
 }
