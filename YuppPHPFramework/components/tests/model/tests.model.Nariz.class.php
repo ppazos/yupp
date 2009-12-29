e0dc23347aacfdc2eb818e20c369e8072594eb86
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2009-12-29 19:56:16 +0000

    Nuevos juevos de tests del ORM

---
+++ YuppPHPFramework/components/tests/model/tests.model.Nariz.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Clase modelo para el test 002.
+ */
+
+class Nariz extends PersistentObject
+{
+   function __construct($args = array (), $isSimpleInstance = false)
+   {
+      $this->withTable = "test_002_nariz";
+
+      $this->addAttribute("tamanio",  Datatypes :: TEXT);
+
+      $this->constraints = array (
+         "tamanio" => array (
+            Constraint :: inList( array("chica", "mediana", "grande") )
+         )
+      );
+
+      parent :: __construct($args, $isSimpleInstance);
+   }
+   public static function listAll($params)
+   {
+      self :: $thisClass = __CLASS__;
+      return PersistentObject :: listAll($params);
+   }
+   public static function count()
+   {
+      self :: $thisClass = __CLASS__;
+      return PersistentObject :: count();
+   }
+   public static function get($id)
+   {
+      self :: $thisClass = __CLASS__;
+      return PersistentObject :: get($id);
+   }
+   public static function findBy(Condition $condition, $params)
+   {
+      self :: $thisClass = __CLASS__;
+      return PersistentObject :: findBy($condition, $params);
+   }
+   public static function countBy(Condition $condition)
+   {
+      self :: $thisClass = __CLASS__;
+      return PersistentObject :: countBy($condition);
+   }
+}
+
+?>
