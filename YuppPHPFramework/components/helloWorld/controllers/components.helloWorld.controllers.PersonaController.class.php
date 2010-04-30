a94e207665e12c12f1788affcaad5e898186bea7
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-04-30 15:50:15 +0000

    Correcciones para el ticket http://code.google.com/p/yupp/issues/detail?id=33

--- YuppPHPFramework/components/helloWorld/controllers/components.helloWorld.controllers.PersonaController.class.php
+++ YuppPHPFramework/components/helloWorld/controllers/components.helloWorld.controllers.PersonaController.class.php
@@ -8,13 +8,28 @@ class PersonaController extends YuppController {
 
     public function indexAction()
     {
-       return $this->listAction();
+        return $this->listAction();
     }
     
     public function showXMLAction()
     {
-       $persona = Persona::get( $this->params['id'] );
-       return $this->renderString( $persona->toXML(true) );
+        $persona = Persona::get( $this->params['id'] );
+        return $this->renderString( $persona->toXML(true) );
+    }
+    
+    public function queryAction()
+    {
+        $q = new Query();
+        $q->addAggregation( SelectAggregation::AGTN_DISTINTC, 'p', 'nombre' )
+          ->addFrom('hello_world_persona', 'p')
+          ->setCondition(
+              Condition::GT('p', "edad", 25)
+            );
+           
+        $pm = PersistentManager::getInstance();
+        $result = $pm->findByQuery( $q );
+       
+        return $this->renderString( print_r($result, true) );
     }
 }
 
