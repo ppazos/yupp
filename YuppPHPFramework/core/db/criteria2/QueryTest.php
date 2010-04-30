496c286acfdb24be48bb33f0a55dc1f05e13e1aa
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-04-30 15:51:44 +0000

    Correcciones para el ticket http://code.google.com/p/yupp/issues/detail?id=33

--- YuppPHPFramework/core/db/criteria2/QueryTest.php
+++ YuppPHPFramework/core/db/criteria2/QueryTest.php
@@ -1,30 +1,28 @@
 <pre>
 <?php
 
-//include_once("core.db.criteria2.Select.class.php");
-
 include_once("core.db.criteria2.Condition.class.php");
 include_once("core.db.criteria2.ComplexCondition.class.php");
 include_once("core.db.criteria2.CompareCondition.class.php");
 include_once("core.db.criteria2.BinaryInfixCondition.class.php");
 include_once("core.db.criteria2.UnaryPrefixCondition.class.php");
 include_once("core.db.criteria2.Query.class.php");
+include_once("core.db.criteria2.Select.class.php");
+
+include_once("../core.db.DatabaseMySQL.class.php");
 
 $q = new Query();
+$q->addFrom('Persona',  'p')
+  ->addFrom('Empleado', 'e')
+  ->setCondition(
+      Condition::_AND()
+        ->add( Condition::EQ('p', "nombre", "Carlos") )
+        ->add( Condition::GT('p', "edad", 5) )
+        ->add( Condition::EQA('p', "nombre", 'p', "nombre2") )
+    );
 
-$q->addFrom('Person',   'per');
-$q->addFrom('Employee', 'emp');
-
-echo $q->evaluate();
-
-echo "<hr/>";
-
-$c = Condition::_AND()
-          ->add( Condition::EQ("per", "nombre", "Carlox") )
-          ->add( Condition::GT("per", "edad", 5) )
-          ->add( Condition::EQA("per", "nombre", "per", "nombre2") );
-
-echo $c->evaluate();
+$db = new DatabaseMySQL();
+echo $db->evaluateQuery( $q );
 
 //echo Condition::EQ("per", "nombre", "Carlox")->evaluate();
 //echo Condition::EQA("per", "nombre", "per", "nombre2")->evaluate();
@@ -38,6 +36,15 @@ $sel->addAvg("per", "age");
 echo $sel->evaluate();
 */
 
+
+$q = new Query();
+$q->addAggregation( SelectAggregation::AGTN_DISTINTC, 'datos', 'nombre' )
+  ->addFrom('Persona',  'datos')
+  ->setCondition(
+      Condition::GT('p', "edad", 5)
+    );
+
+echo $db->evaluateQuery( $q );
 
 ?>
 </pre>
