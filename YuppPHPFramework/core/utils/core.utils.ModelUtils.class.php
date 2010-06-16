4369f5ee23c168a77f621e638d5bd820336063e8
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-06-16 03:33:58 +0000

    Se corrige el metodo getModelClasses para que sea recursivo y soporte subdirectorios en /model.

--- YuppPHPFramework/core/utils/core.utils.ModelUtils.class.php
+++ YuppPHPFramework/core/utils/core.utils.ModelUtils.class.php
@@ -3,9 +3,6 @@
 // Depende de clase FileSystem xq tiene que leer los archivos
 // de un directorio dado para levantar clases (include de esos archivos).
 
-// TODO: Una clase FileSystem que sepa hacer funciones sobre directorios
-// y archivos (ver la clase en SWP_CMS)
-
 /*
  * Created on 24/02/2008
  */
@@ -15,31 +12,46 @@
 //YuppLoader::load("core",'FileSystem');
 //YuppLoader::load("core.config",'PackageNames');
 //YuppLoader::load("core.persistent",'PersistentObject');
-include_once('core/core.FileSystem.class.php');
-include_once('core/config/core.config.PackageNames.class.php');
-include_once('core/persistent/core.persistent.PersistentObject.class.php');
-
-//YuppLoader::load("core.persistent", "PersistentObject");
+include_once('./core/core.FileSystem.class.php');
+include_once('./core/config/core.config.PackageNames.class.php');
+include_once('./core/persistent/core.persistent.PersistentObject.class.php');
 
 class ModelUtils {
 
     /**
-     * Devuelve la lista de los nombres de todas las clases de modelo.
+     * Devuelve la lista de los nombres de todas las clases de modelo de un componente,
+     * incluso si estan en subdirectorios.
      */
-    public static function getModelClasses()
+    public static function getModelClasses( $component )
     {
     	  // el directorio del modelo es fijo, pero lo tengo que sacar de una configuracion, no hardcoded aca.
-        $components = $packNames->getComponentNames();
+        //$components = PackageNames::getComponentNames();
         
         $classNames = array();
-        foreach ( $components as $component )
-        {
+        //foreach ( $components as $component )
+        //{
             $package = "$component/model"; // TODO: si hay subdirectorios, devolverlos tambien.
+            
+            //print_r( $package );
+            //echo "<br/>";
+            
             $model_path = YuppConventions::getModelPath( $package );
-            //$model_path = YuppConventions::getModelPath( $component );
-            $_classNames = FileSystem::getFileNames($model_path, PackageNames::MODEL_FILE_REGEXP, array( PackageNames::MODEL_CLASS_GROUP )); // Todos los php del paquete utils, idem anterior, ahora sin el "utils."
-            $classNames = array_merge($classNames, $_classNames);
-        }
+            
+            //print_r( $model_path );
+            //echo "<br/>";
+            
+            // El componente puede no tener directorio de model
+            //if (file_exists($model_path))
+            //{
+            //   $_classNames = FileSystem::getFileNames($model_path, PackageNames::MODEL_FILE_REGEXP, array( PackageNames::MODEL_CLASS_GROUP )); // Todos los php del paquete utils, idem anterior, ahora sin el "utils."
+            //   $classNames = array_merge($classNames, $_classNames);
+            //}
+            if (file_exists($model_path))
+            {
+               $classNamesRecur = self::getModelClassesRecursive( $model_path );
+               $classNames = array_merge($classNames, $classNamesRecur);
+            }
+        //}
         
         // LEvanta el directorio y los nombres de las clases.
         // Grupo 1 es el nombre del componente
@@ -51,6 +63,40 @@ class ModelUtils {
         // NOMBRES: modelDir/componente.ClassName.php
 
         return $classNames;
+    }
+    
+    /**
+     * Esta devuelve los nombres de las clases dentro del directorio y sigue recorriendo recursivamente.
+     * FIXME: En realidad es una operacion general, tal vez deberia ser parte de FileSystem.
+     */
+    private static function getModelClassesRecursive( $dir )
+    {
+       $res = array();
+       
+       //$entries = FileSystem::getFileNames( $dir ); // subdirs y files
+       
+       $d = dir($dir);
+       while (false !== ($entry = $d->read()))
+       {
+          //echo "$entry<br/>";
+         
+          if ( !String::startsWith($entry, ".") )
+          {
+             if (is_file("$dir/$entry"))
+             {
+                $res[] = $entry;
+             }
+             else if (is_dir("$dir/$entry"))
+             {
+                $res_recur = self:: getModelClassesRecursive( "$dir/$entry" );
+                $res = array_merge($res, $res_recur); 
+             }
+          }
+       }
+       
+       $d->close();
+       
+       return $res;
     }
 
     /**
@@ -134,7 +180,6 @@ class ModelUtils {
      * Devuelve una lista con todos los nombres de los ancestros de clazz hasta PersistentObject.
      * Obs: devuelve las clases en orden de herencia, desde la clase de nivel 1 (hereda directamente de PO) a la ultima subclase. (*)
      */
-    //public function &getAllAncestorsOf( $clazz )
     public function getAllAncestorsOf( $clazz )
     {
         $res = array();
@@ -146,6 +191,28 @@ class ModelUtils {
             $parent = get_parent_class( $parent );
         }
         return $res;
+    }
+    
+    public static function getComponentForModelClass( $classname )
+    {
+       $components = PackageNames::getComponentNames();
+       foreach ( $components as $component )
+       {
+          // FIXME: si la clase esta definida en un subdir de /model no la encuentra.
+         
+          // TODO: que el nombre de la clase se obtenga desde las convenciones, la path tambien.
+          $path = "./components/$component/model/$component.model.$classname.class.php";
+          if ( file_exists( $path ) )
+          {
+             return $component;
+          }
+       }
+       
+       // Puede ser que este en un subdir y no la encuentre...
+       
+       
+       
+       return NULL; // No se encontro
     }
 }
 
