e9b661ef32c2a952db0fcc3e0816c56f8d960812
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-06-16 03:29:03 +0000

    Simploficacion del request y correccion al parseo del response.

--- YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
+++ YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
@@ -1,221 +1,245 @@
 <?php
 
 
-//include_once('../http/core.http.HTTPResponse.class.php');
 YuppLoader::load('core.http', 'HTTPResponse');
 
 class HTTPRequest{
 
-/*	private $code;
-	private $date;
-	private $server;
-	private $powered;
-	private $cookie;
-	private $expires;
-	private $cache;
-	private $pragma;
-	private $content_length;
-	private $connection;
-	private $content_type;
-	*/
-	private $cookie;
-	
-	//Request Método Post
-	public function HttpRequestPost($url){
-		
-		$r = $this->file_post_contents($url);
-		return $r;
-	/*	$res = explode("\r\n", $r);
+/*   private $code;
+   private $date;
+   private $server;
+   private $powered;
+   private $cookie;
+   private $expires;
+   private $cache;
+   private $pragma;
+   private $content_length;
+   private $connection;
+   private $content_type;
+   */
+   private $cookie;
+   
+   //Request Método Post
+   public function HttpRequestPost($url)
+   {
+      $r = $this->file_post_contents($url);
+      return $r;
 
-		return $res[count($res)-1];*/
-	}
-	
-	//Request Método Get
-	public function HttpRequestGet($url){
-	
-		$parsedUrl = parse_url($url);
+   }
+   
+   //Request Método Get
+   public function HttpRequestGet($url)
+   {
+      $parsedUrl = parse_url($url);
 
-		if (!isset($parsedUrl['port'])) {
-			if ($parsedUrl['scheme'] == 'http') 
-			{
-				$parsedUrl['port']=80; 
-			}
-			elseif ($parsedUrl['scheme'] == 'https') 
-			{ 
-				$parsedUrl['port']=443; 
-			}
-		}
-		$parsedUrl['query']=isset($parsedUrl['query'])?$parsedUrl['query']:'';
+      if (!isset($parsedUrl['port'])) {
+         if ($parsedUrl['scheme'] == 'http') 
+         {
+            $parsedUrl['port']=80; 
+         }
+         elseif ($parsedUrl['scheme'] == 'https') 
+         { 
+            $parsedUrl['port']=443; 
+         }
+      }
+      
+      //print_r( $parsedUrl );
+      
+      $parsedUrl['query'] = ( isset( $parsedUrl['query'] ) ? $parsedUrl['query'] : '');
+      $parsedUrl['protocol'] = $parsedUrl['scheme'].'://';
+      
+      $eol = "\r\n";
+      
+      $headers =  "GET ".$url." HTTP/1.0".$eol.
+                  "Host: ".$parsedUrl['host'].$eol;
+                  //"Referer: ".$url['protocol'].$url['host'].$url['path'].$eol.
+      
+      if (isset($this->cookie))
+      {
+         $headers .= "Cookie: ".$this->cookie.$eol;
+      }
+      $headers .= "Connection: close".$eol.$eol;
+      
+//      echo '<textarea style="width:900px; height:200px; border: 1px solid gold;">';
+//      echo $headers;
+//      echo '</textarea>';
+//      
+//      print_r( $parsedUrl );
+//      
+//      echo "<hr/>".gettype($parsedUrl['port'])."<hr/>";
+      
+      $result = '';
+      $timeout = 30;
+      //echo $headers;
+      //$r = file_get_contents($url);
+      $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, 30);
+      if($fp) 
+      {
+         /*
+         fputs($fp, $headers); // Mandar headers
+         while(!feof($fp)) 
+         { 
+            $result .= fgets($fp, 128);
+         }
+         */
+         fwrite($fp, $headers);
+         while(is_resource($fp) && $fp && !feof($fp)) $result .= fread($fp, 1024);
+      }
+      fclose($fp);
+      
+      
+//      $this->_fp = fsockopen(($this->_protocol == 'https' ? 'ssl://' : '') . $this->_host, $this->_port);
+//      fwrite($this->_fp, $req);
+//      while(is_resource($this->_fp) && $this->_fp && !feof($this->_fp)) $response .= fread($this->_fp, 1024);
+//      
+//      fclose($this->_fp);
+      
+      
+//      echo '<textarea style="width:900px; height:300px; border: 1px solid blue;">';
+//      echo $result;
+//      echo '</textarea>';
+      
+      if (!isset($headers)) 
+      {
+         //removes headers
+         $pattern="/^.*\r\n\r\n/s";
+         $result=preg_replace($pattern,'',$result);
 
-		$parsedUrl['protocol']=$parsedUrl['scheme'].'://';
-		$eol="\r\n";
-		$headers =  "GET ".$url." HTTP/1.0".$eol.
-                "Host: ".$parsedUrl['host'];/*
-                "Referer: ".$url['protocol'].$url['host'].$url['path'].$eol.*/
-				"Connection: close\n\n".$eol.
-				$eol;
-		if (isset($this->cookie))
-		{
-			$headers .= $eol.
-				"Cookie: ".$this->cookie.$eol.
-				"Connection: close\n\n".$eol.
-				$eol;
-		}
-		else
-		{
-			$headers .= $eol.
-				"Connection: close\n\n".$eol.
-				$eol;
-		}
-		
-		
-		$timeout = 30;
-		//echo $headers;
-		//$r = file_get_contents($url);
-		$fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, 30);
-		if($fp) 
-		{
-			
-			fputs($fp, $headers);
-			$result='';
-			
-			while(!feof($fp)) 
-			{ 
-				$result .= fgets($fp, 128);
-			}
-			
-		}
-		fclose($fp);
-		if (!$headers) 
-		{
-			//removes headers
-			$pattern="/^.*\r\n\r\n/s";
-			$result=preg_replace($pattern,'',$result);
+      }
 
-		}
+      $r = preg_split('/\r\n/', $result);
+      
+//      echo '<textarea style="width:900px; height:300px; border: 1px solid red;">';
+//      print_r( $r );
+//      echo '</textarea>';
+         
+      $response = new HTTPResponse();
+      $response->createResponse($r);
+      
+//      echo '<textarea style="width:900px; height:300px; border: 1px solid green;">';
+//      print_r( $response );
+//      echo '</textarea>';
+      
+      $responseHeaders = $response->getHeaders();
+         
+      if (isset($responseHeaders['Set-Cookie']))
+      {
+         $this->setCookie($responseHeaders['Set-Cookie']);
+      }
+      
+      return $response;
+   }
+   
 
-		$r = preg_split('/\r\n/', $result);
-			
-		$response = new HTTPResponse();
-		$response->createResponse($r);
-		$responseHeaders = $response->getHeaders();
-			
-		if (isset($responseHeaders['Set-Cookie']))
-		{
-			$this->setCookie($responseHeaders['Set-Cookie']);
-		}
-		
-		return $response;
-		
-	}
-	
+   //auxiliar para envío post
+   private function file_post_contents($url, $headers = false)
+   {
+      $parsedUrl = parse_url($url);
 
-	//auxiliar para envío post
-	private function file_post_contents($url,$headers=false) {
-		$parsedUrl = parse_url($url);
-	//	print_r($parsedUrl['query']);
-
-		if (!isset($parsedUrl['port'])) {
-			if ($parsedUrl['scheme'] == 'http') 
-			{
-				$parsedUrl['port']=80; 
-			}
-			elseif ($parsedUrl['scheme'] == 'https') 
-			{ 
-				$parsedUrl['port']=443; 
-			}
-		}
-	/*	$parsedQuery ='';
-		if (isset($parsedUrl['query']))
-		{
-			$parsedQuery = $this->parseQuery($parsedUrl['query']);
-			//print_r($parsedUrl['query']);	
-		}
-		
-	*/			
-		$parsedUrl['query']=isset($parsedUrl['query'])?$parsedUrl['query']:'';
-
-		$parsedUrl['protocol']=$parsedUrl['scheme'].'://';
-		$eol="\r\n";
-		$headers =  "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol.
-                "Host: ".$parsedUrl['host'].$eol.
-                "Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
-                "Content-Type: application/x-www-form-urlencoded";
-		
-		//si tengo valor en cookie, lo agrego al header
-		if (isset($this->cookie))
-		{
-			$headers .= $eol."Cookie: ".$this->cookie.$eol.
-                "Content-Length: ".strlen($parsedUrl['query']).$eol.
-				$eol.$parsedUrl['query'];
-		}
-		else
-		{
-			$headers .= $eol.
-                "Content-Length: ".strlen($parsedUrl['query']).$eol.
-				$eol.$parsedUrl['query'];
-		}
-	/*	$headers =  "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol.
+      if (!isset($parsedUrl['port']))
+      {
+         if ($parsedUrl['scheme'] == 'http') 
+         {
+            $parsedUrl['port']=80; 
+         }
+         elseif ($parsedUrl['scheme'] == 'https') 
+         { 
+            $parsedUrl['port']=443; 
+         }
+      }
+   /*   $parsedQuery ='';
+      if (isset($parsedUrl['query']))
+      {
+         $parsedQuery = $this->parseQuery($parsedUrl['query']);
+         //print_r($parsedUrl['query']);   
+      }
+      
+   */         
+      
+      $parsedUrl['query']=isset($parsedUrl['query'])?$parsedUrl['query']:'';
+      $parsedUrl['protocol']=$parsedUrl['scheme'].'://';
+      
+      $eol="\r\n";
+      
+      $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].':'.$parsedUrl['port'].$parsedUrl['path']." HTTP/1.0".$eol.
+                 "Host: ".$parsedUrl['host'].$eol.
+                 "Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
+                 "Content-Type: application/x-www-form-urlencoded".$eol;
+      
+      //si tengo valor en cookie, lo agrego al header
+      if (isset($this->cookie))
+      {
+         $headers .= "Cookie: ".$this->cookie.$eol;
+      }
+      $headers .= "Connection: close".$eol;
+      $headers .= "Content-Length: ".strlen($parsedUrl['query']).$eol.$eol;
+      $headers .= $parsedUrl['query'];
+      
+      /*   $headers =  "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol.
                 "Host: ".$parsedUrl['host'].$eol.
                 "Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
                 "Content-Type: application/x-www-form-urlencoded".$eol.
                 "Content-Length: ".strlen($parsedQuery).$eol.
-				$eol.$parsedQuery;*/
-		//echo $headers;
-		$timeout = 30;
-		$fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $timeout);
-		if($fp) 
-		{
-			fputs($fp, $headers);
-			
-			$result = '';
-		
-			while(!feof($fp)) 
-			{ 
-				$result .= fgets($fp, 128);
-			}
+            $eol.$parsedQuery;*/
+      //echo $headers;
+      
+//      echo '<textarea style="width:900px; height:200px; border: 1px solid gold;">';
+//      echo $headers;
+//      echo '</textarea>';
+      
+      $result = '';
+      $timeout = 30;
+      $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $timeout);
+      if($fp) 
+      {
+         /*
+         fputs($fp, $headers);
 
-			fclose($fp);
-			if (!$headers) 
-			{
-				//removes headers
-				$pattern="/^.*\r\n\r\n/s";
-				$result=preg_replace($pattern,'',$result);
-			}
-			
-			$r = preg_split('/\r\n/', $result);
-			
-			$response = new HTTPResponse();
-			$response->createResponse($r);
-			//echo "request";
-			//print_r($response);
-			$responseHeaders = $response->getHeaders();
-			
-			if (isset($responseHeaders['Set-Cookie']))
-			{
-				$this->setCookie($responseHeaders['Set-Cookie']);
-			}
-			return $response;
-		}
-	}
-	
-	/*private function parseQuery($query)
-	{
-		$res = preg_split('/=/', $query);
-		$parsedQuery = array();
-		
-		
-	}*/
-	
-	// Ver si ya tiene cookies, como agregar (por ;)
-	private function setCookie($cookie)
-	{
-		$this->cookie = $cookie;
-	}
-	
-
-	
-	
-		
+         while(!feof($fp)) 
+         { 
+            $result .= fgets($fp, 128);
+         }
+         */
+         fwrite($fp, $headers);
+         while(is_resource($fp) && $fp && !feof($fp)) $result .= fread($fp, 1024);
+      }
+      fclose($fp);
+      
+      if (!$headers) 
+      {
+         //removes headers
+         $pattern="/^.*\r\n\r\n/s";
+         $result=preg_replace($pattern,'',$result);
+      }
+      
+      $r = preg_split('/\r\n/', $result);
+      
+      $response = new HTTPResponse();
+      $response->createResponse($r);
+      //echo "request";
+      //print_r($response);
+      $responseHeaders = $response->getHeaders();
+      
+      if (isset($responseHeaders['Set-Cookie']))
+      {
+         $this->setCookie($responseHeaders['Set-Cookie']);
+      }
+      
+      return $response;
+   }
+   
+   /*private function parseQuery($query)
+   {
+      $res = preg_split('/=/', $query);
+      $parsedQuery = array();
+      
+      
+   }*/
+   
+   // Ver si ya tiene cookies, como agregar (por ;)
+   private function setCookie($cookie)
+   {
+      $this->cookie = $cookie;
+   }
 }
 ?>
--- YuppPHPFramework/core/http/core.http.HTTPResponse.class.php
+++ YuppPHPFramework/core/http/core.http.HTTPResponse.class.php
@@ -1,6 +1,5 @@
 <?php
-class HTTPResponse
-{
+class HTTPResponse {
 
     protected $version;
 
@@ -14,127 +13,130 @@ class HTTPResponse
 	
     public function __construct()
     {
-
     }
-       
     
 	public function createResponse($res = array())
 	{
-		$this->parseResponse($res);
-		//print_r($this);
-		return $this;
-		
-
-	}
-	
-	private function parseResponse($res = array())
-	{
 		//print_r($res);
-		//version, code y message vienen en la primer posición del array
-		$header = $res[0];
-		
-		$headerSplit = preg_split('/ /', $header);
-		
-		$this->version = $headerSplit[0];
-		$this->code = $headerSplit[1];
-		$this->message = '';
-		for ($i = 2; $i <= (count($headerSplit) - 1); $i++)
-		{
-			$this->message .= $headerSplit[$i].' ';	
-		}
-		
-		//guardo el resto de los headers
-		//para cada posicion del array, y mientras la posicion no sea vacio, 
-		// guardo la primer parte como key (antes de ': '), y la segunda posicion como value 
-		$j = 1;
-		while (($j <= (count($res)-1)) && ($res[$j]!= '') )
-		{
-			$header = preg_split('/: /', $res[$j]);
-			$this->headers[$header[0]] = $header[1];
-			$j++;
-		}
-		//avanzo una posicion en el array, para evitar la posicion que no tiene value
-		$j++;
-		while (($j <= (count($res)-1)) )
-		{
-			$this->body .= $res[$j++];
-			$j++;
-		}
-		
+      
+      //version, code y message vienen en la primer posición del array
+      $header = $res[0]; // HTTP/1.1 200 OK
+      
+      $headerSplit = preg_split('/ /', $header);
+      
+      $this->version = $headerSplit[0];
+      $this->code = $headerSplit[1];
+      $this->message = '';
+      for ($i = 2; $i <= (count($headerSplit) - 1); $i++)
+      {
+         $this->message .= $headerSplit[$i].' ';   
+      }
+      
+      //guardo el resto de los headers
+      //para cada posicion del array, y mientras la posicion no sea vacio, 
+      // guardo la primer parte como key (antes de ': '), y la segunda posicion como value 
+      $j = 1;
+      while ( ($j <= (count($res)-1)) && $res[$j]!='' )
+      {
+         $header = preg_split('/: /', $res[$j]);
+         $this->headers[$header[0]] = $header[1];
+         $j++;
+         //echo "<b>$j</b><br/>";
+      }
+      
+      //print_r( $this->headers );
+      
+      //avanzo una posicion en el array, para evitar la posicion que no tiene value
+      //$j++;
+      //echo "<b>$j</b><br/>";
+      while (($j+1 <= (count($res)-1)) )
+      {
+         $this->body .= $res[$j+1];
+         $j++;
+         //echo "$j<br/>";
+      }
+      
+		return $this;
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
 	//get
-	public function getVersion(){
+	public function getVersion()
+   {
 		return $this->version;
 	}
 	
-	public function getStatus(){
+	public function getStatus()
+   {
 		return $this->code;
 	}
 	
-	public function getMessage(){
+	public function getMessage()
+   {
 		return $this->message;
 	}
 	
-	public function getHeaders(){
+	public function getHeaders()
+   {
 		return $this->headers;
 	}
 	
-	public function getBody(){
+	public function getBody()
+   {
 		return $this->body;
 	}
 	
 	//set
-	public function setVersion($version){
+	public function setVersion($version)
+   {
 		$this->version = $version;
 	}
 	
-	public function setCode($code){
+	public function setCode($code)
+   {
 		$this->code = $code;
 	}
 	
-	public function setMessage($message){
+	public function setMessage($message)
+   {
 		$this->message = $message;
 	}
 	
-	public function setHeaders($headers){
+	public function setHeaders($headers)
+   {
 		foreach ($headers as $name => $value) 
 		{
-            if (is_int($name))
-                list($name, $value) = explode(": ", $value, 1);
+         if (is_int($name))
+            list($name, $value) = explode(": ", $value, 1);
 
-            $this->headers[ucwords(strtolower($name))] = $value;
-        }
+         $this->headers[ucwords(strtolower($name))] = $value;
+      }
 	}
 	
-	public function setBody($body){
+	public function setBody($body)
+   {
 		$this->body = $body;
 	}
 	
-    public function isRedirect()
-    {
-        $restype = floor($this->code / 100);
-        if ($restype == 3) 
-        {
-            return true;
-        }
-        return false;
-    }
+   public function isRedirect()
+   {
+      $restype = floor($this->code / 100);
+      if ($restype == 3) 
+      {
+         return true;
+      }
+      return false;
+   }
     
-    //retorna el header pasado como string por parametro 
-    public function getHeader($header)
-    {
-        $header = ucwords(strtolower($header));
-        if (! is_string($header) || ! isset($this->headers[$header])) 
-        	return null;
-        return $this->headers[$header];
-    }
+   //retorna el header pasado como string por parametro 
+   public function getHeader($header)
+   {
+      $header = ucwords(strtolower($header));
+      if (! is_string($header) || ! isset($this->headers[$header])) 
+         return null;
+         
+      return $this->headers[$header];
+   }
 	
 }
 ?>
