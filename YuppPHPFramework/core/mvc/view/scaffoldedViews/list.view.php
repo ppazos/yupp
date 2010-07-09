e06a977d4d5fb5d6b0428726808570503b773f85
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-07-09 15:18:03 +0000

    Agrego que use el helper pager: http://code.google.com/p/yupp/issues/detail?id=48

--- YuppPHPFramework/core/mvc/view/scaffoldedViews/list.view.php
+++ YuppPHPFramework/core/mvc/view/scaffoldedViews/list.view.php
@@ -7,54 +7,38 @@
 $m = Model::getInstance();
 
 ?>
-
 <html>
-<head>
-<style>
-
-table {
-   border: 2px solid #000080;
-   /* spacing: 0px; */
-   border-collapse: separate;
-   border-spacing: 0px;
-}
-
-th {
-   border: 1px solid #000080;
-   padding: 5px;
-   background-color: #000080;
-   color: #fff;
-}
-
-td {
-   border: 1px solid #69c;
-   padding: 5px;
-}
-
-</style>
-</head>
-<body>
-
-<h1>List</h1>
-
-<div align="center"><?php echo $m->flash('message'); ?></div>
-
-[ <a href="create?class=<?php echo $m->get('class') ?>">Create</a> ]<br/><br/>
-
-<?php echo DisplayHelper::model( $m->get('list'), "list", $m->get('class') ); ?>
-
-</br></br>
-
-<?php if ( $m->get('offset')-$m->get('max') >= 0 ) { ?>
-[ <a href="?class=<?php echo $m->get('class') ?>&max=<?php echo $m->get('max'); ?>&offset=<?php echo ($m->get('offset')-$m->get('max')); ?>">Previous</a> ]
-<?php } ?>
-
-<?php echo (int)($m->get('offset')/$m->get('max') + 1); ?> / <?php echo (int)($m->get('count')/$m->get('max') + 1); ?>
-
-<?php if ( $m->get('offset')+$m->get('max') < $m->get('count') ) { ?>
-[ <a href="?class=<?php echo $m->get('class') ?>&max=<?php echo $m->get('max'); ?>&offset=<?php echo ($m->get('offset')+$m->get('max')); ?>">Next</a> ]
-<?php } ?>
-
-
-</body>
+  <head>
+    <style>
+      table {
+         border: 2px solid #000080;
+         /* spacing: 0px; */
+         border-collapse: separate;
+         border-spacing: 0px;
+      }
+      th {
+         border: 1px solid #000080;
+         padding: 5px;
+         background-color: #000080;
+         color: #fff;
+      }
+      td {
+         border: 1px solid #69c;
+         padding: 5px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>List</h1>
+      
+    <div align="center"><?php echo $m->flash('message'); ?></div>
+      
+    [ <a href="create?class=<?php echo $m->get('class') ?>">Create</a> ]<br/><br/>
+      
+    <?php echo DisplayHelper::model( $m->get('list'), "list", $m->get('class') ); ?>
+      
+    </br></br>
+      
+    <?php echo h('pager', array('count'=>$m->get('count'), 'max'=>$m->get('max'), 'offset'=>$m->get('offset'))); ?>
+  </body>
 </html>
