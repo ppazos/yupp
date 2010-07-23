8c550bff5b7eb559687be747b337aa70f5d84568
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-07-23 03:05:11 +0000

    Correcciones para que no de excepcion cuando el response es vacio.

--- YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
+++ YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
@@ -20,7 +20,7 @@ class HTTPRequest {
    private $cookie;
    private $content_type;
    private $timeout = 10; // en segundos
-	
+
    public function setTimeOut( $secs )
    {
       $this->timeout = $secs;
@@ -34,9 +34,13 @@ class HTTPRequest {
       $pattern="/[\t\r\n]*/s";
       $url = preg_replace($pattern,'',$url);
       
+//      echo 'Query HTTPRequestPost B:<br/>';
+//      echo '<textarea style="width: 800px; height: 400px;">'; 
+//      print_r( $url );
+//      echo '</textarea><br/>';
+
       $r = $this->file_post_contents($url);
       return $r;
-
    }
    
    //Request Método Get
@@ -57,16 +61,14 @@ class HTTPRequest {
          }
       }
       
-      //print_r( $parsedUrl );
-      
-      $parsedUrl['query'] = ( isset( $parsedUrl['query'] ) ? $parsedUrl['query'] : '');
+      $parsedUrl['query'] = ( isset($parsedUrl['query']) ? $parsedUrl['query'] : '' );
       $parsedUrl['protocol'] = $parsedUrl['scheme'].'://';
       
       $eol = "\r\n";
       
-      $headers =  "GET ".$url." HTTP/1.0".$eol.
-                  "Host: ".$parsedUrl['host'].$eol;
-                  //"Referer: ".$url['protocol'].$url['host'].$url['path'].$eol.
+      $headers = "GET ".$url." HTTP/1.0".$eol.
+                 "Host: ".$parsedUrl['host'].$eol;
+                 // "Referer: ".$url['protocol'].$url['host'].$url['path'].$eol.
       
       if (isset($this->cookie))
       {
@@ -81,29 +83,29 @@ class HTTPRequest {
 //      print_r( $parsedUrl );
 //      
 //      echo "<hr/>".gettype($parsedUrl['port'])."<hr/>";
-      
+
       $result = '';
-      //echo $headers;
       //$r = file_get_contents($url);
+      // TODO: poder devolver numero de error y errstr.
       $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $this->timeout);
-      if($fp)
+      if ($fp && is_resource($fp))
       {
-         fwrite($fp, $headers); // Envia pedido
-         while(is_resource($fp) && $fp && !feof($fp)) $result .= fread($fp, self::BUFF_SIZE);
+         fputs($fp, $headers); // Envia pedido
+         
+         while (!feof($fp))
+         {
+            $result .= fread($fp, self::BUFF_SIZE);
+         }
       }
-      fclose($fp); // TODO: si hay keepalive, no cerrar el socket...
       
+      fclose($fp); // TODO: si hay keepalive, no cerrar el socket...
+
 // TODO: probar con HTTPS (ssl)
 //      $this->_fp = fsockopen(($this->_protocol == 'https' ? 'ssl://' : '') . $this->_host, $this->_port);
 //      fwrite($this->_fp, $req);
 //      while(is_resource($this->_fp) && $this->_fp && !feof($this->_fp)) $response .= fread($this->_fp, self::BUFF_SIZE);
 //      
 //      fclose($this->_fp);
-      
-      
-//      echo '<textarea style="width:900px; height:300px; border: 1px solid blue;">';
-//      echo $result;
-//      echo '</textarea>';
       
       if (!isset($headers))
       {
@@ -112,21 +114,24 @@ class HTTPRequest {
          $result=preg_replace($pattern,'',$result);
       }
 
-      $r = preg_split('/\r\n/', $result);
-      
-//      echo '<textarea style="width:900px; height:300px; border: 1px solid red;">';
-//      print_r( $r );
-//      echo '</textarea>';
-         
       $response = new HTTPResponse();
-      $response->createResponse($r);
-      
+      if ($result !== "")
+      {
+         $r = preg_split('/\r\n/', $result);
+         $response->createResponse($r);
+      }
+      else // empty response
+      {
+         // http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+         $response->createResponse( array('HTTP/1.1 503 Service Unavailable') );
+      }
+
 //      echo '<textarea style="width:900px; height:300px; border: 1px solid green;">';
 //      print_r( $response );
 //      echo '</textarea>';
-      
+
       $responseHeaders = $response->getHeaders();
-      
+
       if (isset($responseHeaders['Set-Cookie']))
       {
          $this->setCookie($responseHeaders['Set-Cookie']);
@@ -152,24 +157,38 @@ class HTTPRequest {
             $parsedUrl['port']=443;
          }
       }
+
+//      echo "QUERY:<br/>";
+//      echo '<textarea style="width: 800px; height: 400px;">';
+//      echo $url."\n\n"; 
+//      print_r( $parsedUrl['query'] );
+//      echo "</textarea><br/>";
+
    /*   $parsedQuery ='';
       if (isset($parsedUrl['query']))
       {
          $parsedQuery = $this->parseQuery($parsedUrl['query']);
          //print_r($parsedUrl['query']);
       }
-      
    */
+
+      $parsedUrl['query'] = isset($parsedUrl['query'])?$parsedUrl['query']:'';
+      $parsedUrl['protocol'] = $parsedUrl['scheme'].'://';
       
-      $parsedUrl['query']=isset($parsedUrl['query'])?$parsedUrl['query']:'';
-      $parsedUrl['protocol']=$parsedUrl['scheme'].'://';
+      $eol = "\r\n";
       
-      $eol="\r\n";
+      if (!empty($parsedUrl['port']))
+      {
+         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].':'.$parsedUrl['port'].$parsedUrl['path']." HTTP/1.0".$eol;
+      }
+      else
+      {
+         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol;
+      }
       
-      $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].':'.$parsedUrl['port'].$parsedUrl['path']." HTTP/1.0".$eol.
-                 "Host: ".$parsedUrl['host'].$eol.
-                 "Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
-                 "Content-Type: application/x-www-form-urlencoded".$eol;
+      $headers .= "Host: ".$parsedUrl['host'].$eol.
+                  //"Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
+                  "Content-Type: application/x-www-form-urlencoded".$eol;
       
       //si tengo valor en cookie, lo agrego al header
       if (isset($this->cookie))
@@ -179,25 +198,25 @@ class HTTPRequest {
       $headers .= "Connection: close".$eol;
       $headers .= "Content-Length: ".strlen($parsedUrl['query']).$eol.$eol;
       $headers .= $parsedUrl['query'];
-      
-      /*   $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol.
-                "Host: ".$parsedUrl['host'].$eol.
-                "Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
-                "Content-Type: application/x-www-form-urlencoded".$eol.
-                "Content-Length: ".strlen($parsedQuery).$eol.
-            $eol.$parsedQuery;*/
-      //echo $headers;
-      
-//      echo '<textarea style="width:900px; height:200px; border: 1px solid gold;">';
-//      echo $headers;
-//      echo '</textarea>';
-      
+
+/*
+      echo 'Query HTTPRequestPost D:<br/>';
+      echo '<textarea style="width: 800px; height: 400px;">';
+      echo $headers;
+      echo '</textarea><br/>';
+*/
+
       $result = '';
+      // TODO: poder devolver numero de error y errstr.
       $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $this->timeout);
-      if($fp)
+      if($fp && is_resource($fp))
       {
-         fwrite($fp, $headers);
-         while(is_resource($fp) && $fp && !feof($fp)) $result .= fread($fp, self::BUFF_SIZE);
+         fputs($fp, $headers);
+         
+         while (!feof($fp))
+         {
+            $result .= fread($fp, self::BUFF_SIZE);
+         }
       }
       fclose($fp); // TODO: si hay keepalive, no cerrar el socket...
       
@@ -207,15 +226,24 @@ class HTTPRequest {
          $pattern="/^.*\r\n\r\n/s";
          $result=preg_replace($pattern,'',$result);
       }
-      
-      $r = preg_split('/\r\n/', $result);
-      
+
       $response = new HTTPResponse();
-      $response->createResponse($r);
-      //echo "request";
+      if ($result !== "")
+      {
+         $r = preg_split('/\r\n/', $result);
+         $response->createResponse($r);
+      }
+      else // empty response
+      {
+         // http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+         $response->createResponse( array('HTTP/1.1 503 Service Unavailable') );
+      }
+
+      //echo "RESPONSE:<br/>";
       //print_r($response);
-      $responseHeaders = $response->getHeaders();
       
+      $responseHeaders = $response->getHeaders();
+
       if (isset($responseHeaders['Set-Cookie']))
       {
          $this->setCookie($responseHeaders['Set-Cookie']);
--- YuppPHPFramework/core/http/core.http.HTTPResponse.class.php
+++ YuppPHPFramework/core/http/core.http.HTTPResponse.class.php
@@ -1,127 +1,120 @@
 <?php
+
 class HTTPResponse {
 
-    protected $version;
-
-    protected $code;
-
-    protected $message;
-
-    protected $headers = array();
-
-    protected $body;
-	
-    public function __construct()
-    {
-    }
-    
-	public function createResponse($res = array())
-	{
-		//print_r($res);
-      
+   protected $version;
+   protected $code;
+   protected $message;
+   protected $headers = array();
+   protected $body;
+   
+   public function __construct()
+   {
+   }
+   
+   public function createResponse($res = array())
+   {
       //version, code y message vienen en la primer posición del array
       $header = $res[0]; // HTTP/1.1 200 OK
+      
+      if (empty($header)) throw new Exception('La respuesta es vacia '. __FILE__ .' '.__LINE__);
       
       $headerSplit = preg_split('/ /', $header);
       
       $this->version = $headerSplit[0];
-      $this->code = $headerSplit[1];
-      $this->message = '';
-      for ($i = 2; $i <= (count($headerSplit) - 1); $i++)
+      
+      if (isset($headerSplit[1]))
       {
-         $this->message .= $headerSplit[$i].' ';   
-      }
-      
-      //guardo el resto de los headers
-      //para cada posicion del array, y mientras la posicion no sea vacio, 
-      // guardo la primer parte como key (antes de ': '), y la segunda posicion como value 
-      $j = 1;
-      while ( ($j <= (count($res)-1)) && $res[$j]!='' )
-      {
-         $header = preg_split('/: /', $res[$j]);
-         $this->headers[$header[0]] = $header[1];
-         $j++;
-         //echo "<b>$j</b><br/>";
-      }
-      
-      //print_r( $this->headers );
-      
-      //avanzo una posicion en el array, para evitar la posicion que no tiene value
-      //$j++;
-      //echo "<b>$j</b><br/>";
-      while (($j+1 <= (count($res)-1)) )
-      {
-         $this->body .= $res[$j+1];
-         $j++;
-         //echo "$j<br/>";
-      }
-      
-		return $this;
-	}
+         $this->code = $headerSplit[1];
+         $this->message = '';
+         for ($i = 2; $i <= (count($headerSplit) - 1); $i++)
+         {
+            $this->message .= $headerSplit[$i].' ';
+         }
 
+         //guardo el resto de los headers
+         //para cada posicion del array, y mientras la posicion no sea vacio, 
+         // guardo la primer parte como key (antes de ': '), y la segunda posicion como value 
+         $j = 1;
+         while (($j <= (count($res)-1)) && $res[$j] != '')
+         {
+            $header = preg_split('/: /', $res[$j]);
+            $this->headers[$header[0]] = $header[1];
+            $j++;
+         }
 
-	//get
-	public function getVersion()
+         //avanzo una posicion en el array, para evitar la posicion que no tiene value
+         $j++;
+         while (($j <= (count($res)-1)) )
+         {
+            $this->body .= $res[$j++];
+            $j++;
+         }
+      }
+
+      return $this;
+   }
+
+   public function getVersion()
    {
-		return $this->version;
-	}
-	
-	public function getStatus()
+      return $this->version;
+   }
+
+   public function getStatus()
    {
-		return $this->code;
-	}
-	
-	public function getMessage()
+      return $this->code;
+   }
+   
+   public function getMessage()
    {
-		return $this->message;
-	}
-	
-	public function getHeaders()
+      return $this->message;
+   }
+   
+   public function getHeaders()
    {
-		return $this->headers;
-	}
-	
-	public function getBody()
+      return $this->headers;
+   }
+   
+   public function getBody()
    {
-		return $this->body;
-	}
-	
-	//set
-	public function setVersion($version)
+      return $this->body;
+   }
+   
+   public function setVersion($version)
    {
-		$this->version = $version;
-	}
-	
-	public function setCode($code)
+      $this->version = $version;
+   }
+   
+   public function setCode($code)
    {
-		$this->code = $code;
-	}
-	
-	public function setMessage($message)
+      $this->code = $code;
+   }
+   
+   public function setMessage($message)
    {
-		$this->message = $message;
-	}
-	
-	public function setHeaders($headers)
+      $this->message = $message;
+   }
+   
+   public function setHeaders($headers)
    {
-		foreach ($headers as $name => $value) 
-		{
+      foreach ($headers as $name => $value) 
+      {
          if (is_int($name))
             list($name, $value) = explode(": ", $value, 1);
 
          $this->headers[ucwords(strtolower($name))] = $value;
       }
-	}
-	
-	public function setBody($body)
+   }
+   
+   public function setBody($body)
    {
-		$this->body = $body;
-	}
-	
+      $this->body = $body;
+   }
+   
    public function isRedirect()
    {
       $restype = floor($this->code / 100);
-      if ($restype == 3) 
+      if ($restype == 3)
       {
          return true;
       }
@@ -137,6 +130,5 @@ class HTTPResponse {
          
       return $this->headers[$header];
    }
-	
 }
 ?>
