e2f59a838eb83148cdbe0de971ea1edc2989f173
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-12-05 19:52:35 +0000

    se cambia el test de dirty bit para que no haga log a pantalla

--- YuppPHPFramework/apps/tests/tests/apps.tests.tests.TestDirtyBit.class.php
+++ YuppPHPFramework/apps/tests/tests/apps.tests.tests.TestDirtyBit.class.php
@@ -41,10 +41,10 @@ class TestDirtyBit extends TestCase {
    // Test actualizar sin considerar dirtyBit
    public function test2()
    {
-      Logger::getInstance()->on();
+      //Logger::getInstance()->on();
       
-      $timer = new Timer();
-      $timer->start();
+      //$timer = new Timer();
+      //$timer->start();
        
       $c1 = Class1::get(1);      
       $c2 = $c1->getClass2();
@@ -53,21 +53,21 @@ class TestDirtyBit extends TestCase {
       
       $this->assert( $c1->save(), 'Test guardar 1 '. print_r($c1->getErrors(), true));
       
-      $timer->stop();
-      echo 'tiempo total 1: ' . $timer->getElapsedTime() . '<br/>';
+      //$timer->stop();
+      //echo 'tiempo total 1: ' . $timer->getElapsedTime() . '<br/>';
       
       
       // Intento guardar de nuevo la misma instancia, no deberia guardar porque ya se guardo y esta limpia.
       // Observar que este save no hace consultas a la base, y por lo tanto, es mucho mas rapido evitando updates innecesarios.
-      $timer->reset();
-      $timer->start();
+      //$timer->reset();
+      //$timer->start();
       
       $this->assert( $c1->save(), 'Test guardar 2 '. print_r($c1->getErrors(), true));
       
-      $timer->stop();
-      echo 'tiempo total 2: ' . $timer->getElapsedTime() . '<br/>';
+      //$timer->stop();
+      //echo 'tiempo total 2: ' . $timer->getElapsedTime() . '<br/>';
       
-      Logger::getInstance()->off();
+      //Logger::getInstance()->off();
       
       // La nariz NO debe tener errores
       //$this->assert( count($cara->getNariz()->getErrors()) == 0, 'Test errores en nariz 2');
