69251ff33724febc2efbb1be4a1b21ebf0565367
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2010-12-06 18:57:31 +0000

    Se agrega soporte para transformaciones XSL en la generación de XML a partir de clases del modelo.

--- YuppPHPFramework/core/persistent/core.persistent.PersistentObject.class.php
+++ YuppPHPFramework/core/persistent/core.persistent.PersistentObject.class.php
@@ -878,6 +878,8 @@ class PersistentObject {
       $valid = true;
       $this->errors = NULL; // Reset
 
+      // FIXME: el primer foreach deberia hacerse sobre $attrs
+
       if ($this->constraints)
       {
         foreach ( $this->constraints as $attr => $constraintArray ) // Para cada campo
@@ -2170,8 +2172,120 @@ $err = ValidationMessage::getMessage( $constraint, $attr, $this->aGet($attr) );
       return $this->xml->outputMemory();
    }
    
+   
+   // =================================================================================================
+   /*
+    * para obtener el xslt, hacer:
+    * 
+    * $xsl_dom = new DOMDocument();
+    * $xsl_dom->load( 'yuppxml2impxml.xsl', LIBXML_NOCDATA);
+    * 
+    * $xslt = new XSLTProcessor();
+    * $xslt->importStylesheet( $xsl_dom );
+    */ 
+   public function toXML2( $recursive = false, $pretty = false, XSLTProcessor $xslt = NULL )
+   {
+      //$xml_dom = new DOMDocument();
+      //$xml_dom = new DOMDocument('1.0', 'utf-8');
+      $xml_dom = new DOMDocument('1.0', 'iso-8859-1');
+      
+      if ($pretty)
+      {
+         $xml_dom->preserveWhiteSpace = false;
+         $xml_dom->formatOutput   = true;
+      }
+      
+      $loopDetection = new ArrayObject();
+
+      $this->toXMLSingle2( $this, $xml_dom, $xml_dom, $recursive, $loopDetection );
+      
+      if ($xslt === NULL)
+         return $xml_dom->saveXML();
+      else
+         return $xslt->transformToXML( $xml_dom ); 
+   }
+   
+   // PersistentObject, DomDocument, DomNode, Boolean, ArrayObject, String
+   private function toXMLSingle2( $obj, $xml_dom_doc, $xml_parent_node, $recursive, $loopDetection, $attrName = null )
+   {
+      if(!in_array(get_class($obj).'_'.$obj->getId(), (array)$loopDetection)) // si no esta marcado como recorrido
+      {
+         $loopDetection[] = get_class($obj).'_'.$obj->getId(); // Marca como recorrido
+         
+         // Nodo actual
+         $node = NULL;
+         
+         // El nombre de la tag es el nombre del atributo, a no ser que sea el nodo raiz.
+         if ( is_null($attrName) )
+         {
+            $node = $xml_dom_doc->createElement( get_class($obj) );
+            // $this->xml->startElement( get_class($obj) );
+         }
+         else
+         {
+            //$this->xml->startElement( $attrName );
+            //$this->xml->writeAttribute( 'type', get_class($obj) );
+            
+            //$node = $xml_dom_doc->createElement( get_class($obj) );
+            $node = $xml_dom_doc->createElement( $attrName );
+            $node->setAttribute( 'type', get_class($obj) ); // setAttribute ( string $name , string $value )
+         }
+         
+         // Para la primer llamada, este nodo es el dom_document
+         $xml_parent_node->appendChild( $node );
+         
+         foreach ( $obj->attributeTypes as $attr => $type )
+         {
+            //$this->xml->writeElement($attr, $obj->aGet($attr));
+            $attr_node = $xml_dom_doc->createElement( $attr, $obj->aGet($attr) );
+            $node->appendChild( $attr_node );
+         }
+         
+         if ($recursive)
+         {
+            foreach ($obj->getHasOne() as $attr => $clazz)
+            {
+               $relObj = $obj->aGet($attr);
+               if (!is_null($relObj))
+               {
+                  if(!in_array(get_class($relObj).'_'.$relObj->getId(), (array)$loopDetection)) // si no esta marcado como recorrido
+                  {
+                     // FIXME: las tags de los atributos hijos de la instancia raiz deberian
+                     //        tener su nombre igual al nombre del atributo, no el nombre de
+                     //        la clase. Con este codigo es el nombre de la clase.
+                     $this->toXMLSingle2( $relObj, $xml_dom_doc, $node, $recursive, $loopDetection, $attr );
+                  }
+               }
+            }
+            
+            foreach ($obj->getHasMany() as $attr => $clazz)
+            {
+               //$xml_dom_doc->startElement( $attr );
+               //$xml_dom_doc->writeAttribute( 'type', $obj->getHasManyType($attr) ); // list, colection, set
+              
+               $hm_node = $xml_dom_doc->createElement( $attr );
+               $hm_node->setAttribute( 'type', $obj->getHasManyType($attr) ); // list, colection, set
+               
+               $relObjs = $obj->aGet($attr);
+               
+               foreach ($relObjs as $relObj)
+               {
+                  if(!in_array(get_class($relObj).'_'.$relObj->getId(), (array)$loopDetection)) // si no esta marcado como recorrido
+                  {
+                     $this->toXMLSingle2($relObj, $xml_dom_doc, $hm_node, $recursive, $loopDetection);
+                  }
+               }
+               
+               $node->appendChild( $hm_node );
+            }
+         } // si es recursiva
+      } // si no hay loop
+   }
+   // =================================================================================================
+   
+   
    private function toXMLSingle( $obj, $recursive, $loopDetection, $attrName = null )
-   {  
+   {
       if(!in_array(get_class($obj).'_'.$obj->getId(), (array)$loopDetection)) // si no esta marcado como recorrido
       {
          $loopDetection[] = get_class($obj).'_'.$obj->getId(); // Marca como recorrido
@@ -2212,21 +2326,12 @@ $err = ValidationMessage::getMessage( $constraint, $attr, $this->aGet($attr) );
                }
             }
             
-            //echo get_class($obj) . "hasMany: ";
-            //print_r( $obj->getHasMany() );
-            
             foreach ($obj->getHasMany() as $attr => $clazz)
             {
-               //echo "attr: $attr<br/>";
-               //echo $obj->getHasManyType($attr) . "<br/>";
-               
-               //$this->xml->startElement( $obj->getHasManyType($attr) ); // list, colection, set
                $this->xml->startElement( $attr );
                $this->xml->writeAttribute( 'type', $obj->getHasManyType($attr) ); // list, colection, set
                
                $relObjs = $obj->aGet($attr);
-               
-               //print_r($relObjs);
                
                foreach ($relObjs as $relObj)
                {
