87db5ef8b5a59b05ab9b052f7896c5753a633764
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2011-02-11 23:58:41 +0000

    Se mejora la vista de ajaxLinkTest para seleccionar librería javascript entre prototype y jquery.

--- YuppPHPFramework/apps/tests/views/helpersTest/ajaxLinkTest.view.php
+++ YuppPHPFramework/apps/tests/views/helpersTest/ajaxLinkTest.view.php
@@ -3,58 +3,71 @@
 $m = Model::getInstance();
 
 ?>
-
 <html>
   <head>
     <title>Helpers Test: Ajax Link Test</title>
     
-    <?php /*echo h("js", array("name" => "prototype_161") ); */ ?>
-    <?php echo h("js", array("name" => "jquery/jquery-1.3.1.min") ); ?>
-    
-    <script type="text/javascript">
-     // Handlers para Prototype
-     /*
-     var before_function = function(req, json) {
-       
-       $('content_div').innerHTML = "Cargando...";
-     }
-     var after_function = function(req, json) {
+    <?php if ($m->get('jslib')=='prototype') : ?>
+      <?php echo h("js", array("name" => "prototype_161") ); ?>
       
-       if (!json) json = req.responseText.evalJSON();
-       else       json = json.evalJSON();
+      <script type="text/javascript">
+      
+        // Handlers para Prototype
+        var before_function = function(req, json) {
+           
+          $('content_div').innerHTML = "Cargando...";
+        }
+        var after_function = function(req, json) {
+          
+          if (!json) json = req.responseText.evalJSON();
+          else       json = json.evalJSON();
+            
+          $('content_div').innerHTML = json.mensaje;
+        }
+      </script>
+      
+    <?php else : ?>
+      <?php echo h("js", array("name" => "jquery/jquery-1.3.1.min") ); ?>
+    
+      <script type="text/javascript">
+      
+        // Handlers para JQuery
+        var before_function = function(req, json) {
+           
+          $('#content_div').html( "Cargando..." );
+        }
+        var after_function = function(json) {
+          
+          //alert(json);
+          //alert(json.mensaje); // undefined
+           
+          var obj = eval('('+json+')');
+           
+          //alert(obj.mensaje); // Hola mundo!
+           
+          $('#content_div').html( obj.mensaje );
+        }
+      </script>
+    <?php endif; ?>
         
-       $('content_div').innerHTML = json.mensaje;
-     }
-     */
-    </script>
-    
-    <script type="text/javascript">
-     // Handlers para JQuery
-     var before_function = function(req, json) {
-       
-       $('#content_div').html( "Cargando..." );
-     }
-     var after_function = function(json) {
-      
-       //alert(json);
-       //alert(json.mensaje); // undefined
-       
-       var obj = eval('('+json+')');
-       
-       //alert(obj.mensaje); // Hola mundo!
-       
-       $('#content_div').html( obj.mensaje );
-     }
-    </script>
   </head>
   <body>
     <h1>Helpers Test: Ajax Link Test</h1>
     
+    <form>
+      <select name="jslib">
+        <option value="jquery" <?php echo (( in_array($m->get('jslib'), array(NULL, '', 'jquery')) )?'selected="true"':''); ?>>jquery</option>
+        <option value="prototype" <?php echo (( $m->get('jslib')=='prototype' )?'selected="true"':''); ?>>prototype</option>
+      </select>
+      <input type="submit" value="seleccionar libreria" />
+    </form>
+    
     <?php echo h("ajax_link", array("action" => "ajaxLinkTest",
-                               "doit"   => "true",
-                               "body"   => "haz clic en el link",
-                               "after"  => "after_function",
-                               "before" => "before_function" ) ); ?>
+                                    "doit"   => "true",
+                                    "body"   => "haz clic en el link",
+                                    "after"  => "after_function",
+                                    "before" => "before_function",
+                                    "attrs"  => array("class"=>"link") ) ); ?>
     <br/><br/>
    
     <div style="width: 500px; height: 200px; padding:10px; padding-right:10px; background-color: #ffff80; border: 1px dashed #000" id="content_div">
