0f28c850d182f88dadcc524a3592459d2532a7af
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2011-02-20 04:54:45 +0000

    Se corrigen bugs.

--- YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
+++ YuppPHPFramework/core/http/core.http.HTTPRequest.class.php
@@ -28,19 +28,157 @@ class HTTPRequest {
    
    //Request Método Post
    // FIXME: cambiarle el nombre a post
-   public function HttpRequestPost($url)
+   public function HttpRequestPost($url, $params = array())
    {
+      // FIXME: si $url ya tiene params (?a=e), adjuntar los params de $params
+    
       // Los espacios y otros caracteres de separacion no son admisibles en una url...
       $pattern="/[\t\r\n]*/s";
-      $url = preg_replace($pattern,'',$url);
+      $url = preg_replace($pattern, '', $url);
       
 //      echo 'Query HTTPRequestPost B:<br/>';
 //      echo '<textarea style="width: 800px; height: 400px;">'; 
 //      print_r( $url );
 //      echo '</textarea><br/>';
 
-      $r = $this->file_post_contents($url);
-      return $r;
+      //$r = $this->file_post_contents($url);
+      //return $r;
+      
+      $parsedUrl = parse_url($url); // FIXME: no deberia hacer parse de la url con params, los params deberian venir aparte.
+
+      if (!isset($parsedUrl['port']))
+      {
+         if ($parsedUrl['scheme'] == 'http')
+         {
+            $parsedUrl['port']=80;
+         }
+         elseif ($parsedUrl['scheme'] == 'https')
+         {
+            $parsedUrl['port']=443;
+         }
+      }
+
+//      echo "QUERY:<br/>";
+//      echo '<textarea style="width: 800px; height: 400px;">';
+//      echo $url."\n\n"; 
+//      print_r( $parsedUrl['query'] );
+//      echo "</textarea><br/>";
+
+   /*   $parsedQuery ='';
+      if (isset($parsedUrl['query']))
+      {
+         $parsedQuery = $this->parseQuery($parsedUrl['query']);
+         //print_r($parsedUrl['query']);
+      }
+   */
+
+      //$parsedUrl['query'] = isset($parsedUrl['query'])?$parsedUrl['query']:'';
+      
+      // Adjunta los params de $params si ya tienen params en la url, si no,
+      // los mete como nuevos.
+      $moreParams = '';
+      if (isset($parsedUrl['query']))
+      {
+         $moreParams = '&';
+      }
+      else
+      {
+         $parsedUrl['query'] = '';
+      }
+      foreach ($params as $name=>$value)
+      {
+         $moreParams .= $name.'='.$value.'&';
+      }
+      $parsedUrl['query'] .= substr($moreParams, 0, -1);
+      
+      
+      $parsedUrl['protocol'] = $parsedUrl['scheme'].'://';
+      
+      $eol = "\r\n";
+      
+      if (!isset($parsedUrl['path'])) $parsedUrl['path'] = '';
+      
+      if (!empty($parsedUrl['port']))
+      {
+         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].':'.$parsedUrl['port'].$parsedUrl['path']." HTTP/1.0".$eol;
+      }
+      else
+      {
+         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol;
+      }
+      
+      $headers .= "Host: ".$parsedUrl['host'].$eol.
+                  //"Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
+                  "Content-Type: application/x-www-form-urlencoded".$eol;
+      
+      //si tengo valor en cookie, lo agrego al header
+      if (isset($this->cookie))
+      {
+         $headers .= "Cookie: ".$this->cookie.$eol;
+      }
+      $headers .= "Connection: close".$eol;
+      $headers .= "Content-Length: ".strlen($parsedUrl['query']).$eol.$eol;
+      $headers .= $parsedUrl['query'];
+
+/*
+      echo 'Query HTTPRequestPost D:<br/>';
+      echo '<textarea style="width: 800px; height: 400px;">';
+      echo $headers;
+      echo '</textarea><br/>';
+*/
+
+      $response = new HTTPResponse();
+      $result = '';
+      try
+      {
+         // TODO: poder devolver numero de error y errstr.
+         $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $this->timeout);
+         if($fp && is_resource($fp))
+         {
+            fputs($fp, $headers);
+
+            while (!feof($fp))
+            {
+               $result .= fread($fp, self::BUFF_SIZE);
+            }
+         }
+         fclose($fp); // TODO: si hay keepalive, no cerrar el socket...
+      }
+      catch (Exception $e)
+      {
+         $response->createResponse( array('HTTP/1.1 404 Not Found') );
+      }
+
+      // Esto de que sirve? siempre da false y nunca entra porque headers siempre tiene algo.
+      if (!isset($headers))
+      {
+         //removes headers
+         $pattern="/^.*\r\n\r\n/s";
+         $result=preg_replace($pattern,'',$result);
+      }
+
+      if ($result !== '')
+      {
+         $r = preg_split('/\r\n/', $result);
+         $response->createResponse($r);
+      }
+      else // empty response
+      {
+         // http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+         $response->createResponse( array('HTTP/1.1 503 Service Unavailable') );
+      }
+
+      //echo "RESPONSE:<br/>";
+      //print_r($response);
+      
+      $responseHeaders = $response->getHeaders();
+
+      if (isset($responseHeaders['Set-Cookie']))
+      {
+         $this->setCookie($responseHeaders['Set-Cookie']);
+      }
+      
+      return $response;
    }
    
    //Request Método Get
@@ -138,126 +276,6 @@ class HTTPRequest {
 //      print_r( $response );
 //      echo '</textarea>';
 
-      $responseHeaders = $response->getHeaders();
-
-      if (isset($responseHeaders['Set-Cookie']))
-      {
-         $this->setCookie($responseHeaders['Set-Cookie']);
-      }
-      
-      return $response;
-   }
-   
-
-   //auxiliar para envío post
-   private function file_post_contents($url, $headers = false)
-   {
-      $parsedUrl = parse_url($url); // FIXME: no deberia hacer parse de la url con params, los params deberian venir aparte.
-
-      if (!isset($parsedUrl['port']))
-      {
-         if ($parsedUrl['scheme'] == 'http')
-         {
-            $parsedUrl['port']=80;
-         }
-         elseif ($parsedUrl['scheme'] == 'https')
-         {
-            $parsedUrl['port']=443;
-         }
-      }
-
-//      echo "QUERY:<br/>";
-//      echo '<textarea style="width: 800px; height: 400px;">';
-//      echo $url."\n\n"; 
-//      print_r( $parsedUrl['query'] );
-//      echo "</textarea><br/>";
-
-   /*   $parsedQuery ='';
-      if (isset($parsedUrl['query']))
-      {
-         $parsedQuery = $this->parseQuery($parsedUrl['query']);
-         //print_r($parsedUrl['query']);
-      }
-   */
-
-      $parsedUrl['query'] = isset($parsedUrl['query'])?$parsedUrl['query']:'';
-      $parsedUrl['protocol'] = $parsedUrl['scheme'].'://';
-      
-      $eol = "\r\n";
-      
-      if (!empty($parsedUrl['port']))
-      {
-         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].':'.$parsedUrl['port'].$parsedUrl['path']." HTTP/1.0".$eol;
-      }
-      else
-      {
-         $headers = "POST ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path']." HTTP/1.0".$eol;
-      }
-      
-      $headers .= "Host: ".$parsedUrl['host'].$eol.
-                  //"Referer: ".$parsedUrl['protocol'].$parsedUrl['host'].$parsedUrl['path'].$eol.
-                  "Content-Type: application/x-www-form-urlencoded".$eol;
-      
-      //si tengo valor en cookie, lo agrego al header
-      if (isset($this->cookie))
-      {
-         $headers .= "Cookie: ".$this->cookie.$eol;
-      }
-      $headers .= "Connection: close".$eol;
-      $headers .= "Content-Length: ".strlen($parsedUrl['query']).$eol.$eol;
-      $headers .= $parsedUrl['query'];
-
-/*
-      echo 'Query HTTPRequestPost D:<br/>';
-      echo '<textarea style="width: 800px; height: 400px;">';
-      echo $headers;
-      echo '</textarea><br/>';
-*/
-
-      $response = new HTTPResponse();
-      $result = '';
-      try
-      {
-         // TODO: poder devolver numero de error y errstr.
-         $fp = fsockopen($parsedUrl['host'], $parsedUrl['port'], $errno, $errstr, $this->timeout);
-         if($fp && is_resource($fp))
-         {
-            fputs($fp, $headers);
-
-            while (!feof($fp))
-            {
-               $result .= fread($fp, self::BUFF_SIZE);
-            }
-         }
-         fclose($fp); // TODO: si hay keepalive, no cerrar el socket...
-      }
-      catch (Exception $e)
-      {
-         $response->createResponse( array('HTTP/1.1 404 Not Found') );
-      }
-
-      // Esto de que sirve? siempre da false y nunca entra porque headers siempre tiene algo.
-      if (!isset($headers))
-      {
-         //removes headers
-         $pattern="/^.*\r\n\r\n/s";
-         $result=preg_replace($pattern,'',$result);
-      }
-
-      if ($result !== '')
-      {
-         $r = preg_split('/\r\n/', $result);
-         $response->createResponse($r);
-      }
-      else // empty response
-      {
-         // http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-         $response->createResponse( array('HTTP/1.1 503 Service Unavailable') );
-      }
-
-      //echo "RESPONSE:<br/>";
-      //print_r($response);
-      
       $responseHeaders = $response->getHeaders();
 
       if (isset($responseHeaders['Set-Cookie']))
