5c0eb3fc5b002fdd8e7012b467dfef17bbb065bd
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2011-02-23 00:35:20 +0000

    Mejoras a Yupp Desktop: filtro de aplicaciones

--- YuppPHPFramework/core/mvc/view/scaffoldedViews/index.view.php
+++ YuppPHPFramework/core/mvc/view/scaffoldedViews/index.view.php
@@ -10,53 +10,56 @@ global $_base_dir;
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    
+    <?php echo h('js', array('name'=>'jquery/jquery-1.5.min')); ?>
+    
     <style>
       body {
-         font-family: arial, verdana, tahoma;
-         font-size: 12px;
-         background-color: #efefef;
+        font-family: arial, verdana, tahoma;
+        font-size: 12px;
+        background-color: #efefef;
       }
       h1 {
-         margin: 0px;
-         padding-top: 35px;
-         display: inline-block;
+        margin: 0px;
+        padding-top: 35px;
+        display: inline-block;
       }
       table, tr, td {
-         margin: 0px;
-         padding: 0px;
-         border: 0px;
+        margin: 0px;
+        padding: 0px;
+        border: 0px;
       }
       #actions {
-          background: #fff url(<?php echo $_base_dir; ?>/images/shadow.jpg) bottom repeat-x;
-          border: 1px solid #ccc;
-          border-style: solid none solid none;
-          padding: 7px 12px;
+        background: #fff url(<?php echo $_base_dir; ?>/images/shadow.jpg) bottom repeat-x;
+        border: 1px solid #ccc;
+        border-style: solid none solid none;
+        padding: 7px 12px;
       }
       #apps {
-         background-color: #fff;
-         padding: 10px;
+        background-color: #fff;
+        padding: 10px;
       }
       .menu_btn {
-         padding: 15px;
-         padding-left: 20px;
-         padding-right: 20px;
-         margin-top: 5px;
-         margin-left: 5px;
-         /*width: 110px;*/
-         text-align: center;
+        padding: 15px;
+        padding-left: 20px;
+        padding-right: 20px;
+        argin-top: 5px;
+        margin-left: 5px;
+        /*width: 110px;*/
+        text-align: center;
       }
       .menu_btn.active {
-         background-color: #fff; /* tab activa del menu superior */
+        background-color: #fff; /* tab activa del menu superior */
       }
       .menu_btn img {
-         border: 0px;
+        border: 0px;
       }
       #apps ul {
-         margin: 0px;
-         padding: 0px;
-         /*position: relative;*/
-         /*left: 0px;*/
-         list-style: none;
+        margin: 0px;
+        padding: 0px;
+        /*position: relative;*/
+        /*left: 0px;*/
+        list-style: none;
       }
       #apps li {
        width: 250px;
@@ -76,52 +79,105 @@ global $_base_dir;
        background-color: #ffff80;
       }
       #apps li:hover {
-         background-color: #99ddff;
-         /*cursor: pointer;*/
+        background-color: #99ddff;
+        /*cursor: pointer;*/
       }
       #apps li a {
-         /*height: 100%;*/
-         display: block;
-         /*text-decoration: none;*/
-         /*color: #000;*/
+        /*height: 100%;*/
+        display: block;
+        /*text-decoration: none;*/
+        /*color: #000;*/
       }
       .app_icon {
-         display: inline-block;
-         vertical-align: top;
-         width: 64px;
-         margin: 0px;
-         margin-right: 3px;
-         padding: 0px;
-         float: left;
+        display: inline-block;
+        vertical-align: top;
+        width: 64px;
+        margin: 0px;
+        margin-right: 3px;
+        padding: 0px;
+        float: left;
       }
       .app_icon img {
          border: 0px;
       }
       .app_details {
-         display: inline-block;
-         vertical-align: top;
-         width: 181px;
-         /*border: 1px solid #000;*/
-         margin: 0px;
-         padding: 0px;
-         padding-top: 3px;
-         float: left;
+        display: inline-block;
+        vertical-align: top;
+        width: 181px;
+        /*border: 1px solid #000;*/
+        margin: 0px;
+        padding: 0px;
+        padding-top: 3px;
+        float: left;
       }
       table#top {
-         width: 100%;
+        width: 100%;
       }
       td#logo {
-         /*display: inline-block;*/
-         width: 64px;
+        /*display: inline-block;*/
+        width: 64px;
       }
       td#top_right {
-         /*width: 100%;*/
-         /*border: 1px solid #000;*/
+        /*width: 100%;*/
+        /*border: 1px solid #000;*/
       }
       .right {
-         float: right;
+        float: right;
+      }
+      
+      /** App Filters **/
+      .app_filter {
+        padding: 3px; /* Para hacer mas faciles de cliquear los links con 1 letra */
+      }
+      .highlight {
+        font-weight: bold;
       }
     </style>
+    <script type="text/javascript">
+    
+      var selectedLink;
+    
+      $(document).ready( function() {
+        
+        /*
+        $.each( $('.app'), function (i, l) {
+          //alert( $(l).attr('class') );
+        });
+        */
+        
+        $('.app_filter').click( function()
+        {
+          link = $(this);
+          letra = link.attr('class').substr(11); // trato a la class "app_filter letra" como string para sacar la letra
+          //letra = link.attr('class');
+          
+          //alert('='+letra+"=");
+          filter(letra.toLowerCase());
+          
+          if (selectedLink) selectedLink.removeClass('highlight');
+          link.addClass('highlight');
+          selectedLink = link;
+        });
+        
+      });
+      
+      function filter(letra)
+      {
+        //alert('letra: '+ letra);
+        $.each( $('.app'), function (idx, elem)
+        {
+          app = $(elem);
+          
+          if (letra == 'all') app.show();
+          else
+          {
+            appName = app.attr('class').substr(4); // Trato a la class 'app appName' como un string para obtener el appName
+            if (letra != appName[0].toLowerCase()) app.hide();
+            else app.show();
+          }
+        });
+      }
+    </script>
   </head>
   <body>
     <table id="top" cellspacing="0">
@@ -159,7 +215,16 @@ global $_base_dir;
                    'component'=>'core',
                    'controller'=>'core',
                    'action'=>'createApp',
-                   'body'=>'Nueva Aplicacion'));?>
+                   'body'=>'Nueva Aplicacion'));?> | 
+      
+      <?php
+        // TODO: hacerlo inteligente: solo mostrar letras de aplicacioens existentes.
+        $letras = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','X','Y','Z');
+      ?>
+      <a href="#" class="app_filter all">Todas</a>
+      <?php foreach ($letras as $letra) : ?>
+        <a href="#" class="app_filter <?php echo $letra; ?>"><?php echo $letra; ?></a>
+      <?php endforeach; ?>
     </div>
     
     <div id="apps">
@@ -167,7 +232,7 @@ global $_base_dir;
     
       <ul>
         <?php foreach ($apps as $app) : ?>
-          <li>
+          <li class="app <?php echo $app->getName(); ?>">
               <div class="app_icon">
                 <a href="<?php echo h('url', array(
                             'component'=>$app->getName(),
