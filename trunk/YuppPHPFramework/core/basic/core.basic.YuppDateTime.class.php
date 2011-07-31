87d0dd85bc13f42f14d70218d39d339b8f10f16c
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2011-07-31 21:15:31 +0000

    Se corrige formato del codigo

--- trunk/YuppPHPFramework/core/basic/core.basic.YuppDateTime.class.php
+++ trunk/YuppPHPFramework/core/basic/core.basic.YuppDateTime.class.php
@@ -1,9 +1,10 @@
 <?php
-class YuppDateTime
-{
+
+class YuppDateTime {
+   
    const MySQL_DATETIME_FORMAT  = "Y-m-d H:i:s";
-   const MySQL_DATE_FORMAT      = "Y-m-d";
-   const MySQL_TIME_FORMAT      = "H:i:s";
+   const MySQL_DATE_FORMAT     = "Y-m-d";
+   const MySQL_TIME_FORMAT     = "H:i:s";
    
    public static function dateParts( $date )
    {
@@ -12,7 +13,7 @@ class YuppDateTime
       $arr['year']  = (int)$arr[0];
       $arr['month'] = (int)$arr[1];
       $arr['day']   = (int)$arr[2];
-      
+     
       return $arr;
    }
    
@@ -25,32 +26,32 @@ class YuppDateTime
          // Verifica validez de la fecha
          if(checkdate($parts[2],$parts[3],$parts[1])) return true;
       }
-     
+    
       return false;
    }
    
    public static function checkMySQLDateTime($mysql_date_time)
    {
       $arr = explode( " ", $mysql_date_time );
-      
+     
       // Para ser datetime debe tener date y time
       if (count($arr)!=2) return false;
-      
+     
       $date = $arr[0];
       $time = $arr[1];
-      
+     
       // Verifica la parte date
       if (self::checkMySQLDate($date))
       {
          // Verifica la parte time
          $arr = explode( ":", $time );
-         
+       
          // Horas y minutos obligatorios, segundos opcionales
          if (count($arr)<2) return false;
-         
+       
          $hour = (int)$arr[0];
          $mins = (int)$arr[1];
-         
+       
          $segs = 0;
          if (isset($arr[2])) $segs = (int)$arr[2];
 
@@ -60,19 +61,20 @@ class YuppDateTime
 
    function checktime($hour, $minute)
    {
-    if ($hour > -1 && $hour < 24 && $minute > -1 && $minute < 60) {
-        return true;
-    }
-} 
+      if ($hour > -1 && $hour < 24 && $minute > -1 && $minute < 60)
+      {
+         return true;
+      }
+   } 
 
-	public static function timeToMySQLDate($timestamp)
-	{
+   public static function timeToMySQLDate($timestamp)
+   {
       return date(self::MySQL_DATE_FORMAT, $timestamp);
-	}
+   }
    
    public static function timeToMySQLDateTime($timestamp)
    {
-   	return date(self::MySQL_DATETIME_FORMAT, $timestamp);
+      return date(self::MySQL_DATETIME_FORMAT, $timestamp);
    }
    
    public static function timeToMySQLTime($timestamp)
@@ -82,20 +84,20 @@ class YuppDateTime
    
    public static function mySQLDateToTime( $mysql_date ) 
    {
-      list($year, $month, $day) = split('-', $mysql_date);
-      
-      $diasMes = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-      
-      echo "YEAR $year<br/>";
-      echo "MONTH $month<br/>";
-      echo "DAY $day<br/>";
-      
-      $segundosPorDiasDeMeses = 0;
-      for($i=0; $i<$month; $i++) $segundosPorDiasDeMeses += $diasMes[$i] * 24 * 60 * 60;
-      
-      // FIXME: dias del mes! dependen del mes! le puse 30!
-      // Esto da parecido pero esta por debajo del tiempo real! es porque le falta el tiempo! (horas, minutos, segundos)
-      return (($year-1970) * 365 * 24 * 60 * 60) +
+     list($year, $month, $day) = split('-', $mysql_date);
+     
+     $diasMes = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+     
+     echo "YEAR $year<br/>";
+     echo "MONTH $month<br/>";
+     echo "DAY $day<br/>";
+     
+     $segundosPorDiasDeMeses = 0;
+     for($i=0; $i<$month; $i++) $segundosPorDiasDeMeses += $diasMes[$i] * 24 * 60 * 60;
+     
+     // FIXME: dias del mes! dependen del mes! le puse 30!
+     // Esto da parecido pero esta por debajo del tiempo real! es porque le falta el tiempo! (horas, minutos, segundos)
+     return (($year-1970) * 365 * 24 * 60 * 60) +
              $segundosPorDiasDeMeses + 
              //(($month-1) * 30 * 24 * 60 * 60) + // -1 porque el mes actual no termina todavia! LOS DIAS DEBERIAN SER DISTINTOS PARA CADA MES!
              (($day-1) * 24 * 60 * 60); // si es solo date, claramente van a faltar minutos y segundos para la cuenta!
