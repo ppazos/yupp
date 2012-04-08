ac1e9083c92518d1ce594e27fc5c7f5b339dddcd
pablo.swp <pablo.swp@31eb27a2-bd50-0410-acf8-83c11916ec22>
2012-04-08 09:23:12 +0000

    se mejora el test para verificar transaccionalidad.

--- YuppPHPFramework/apps/tests/tests/apps.tests.tests.TestIssue37.class.php
+++ YuppPHPFramework/apps/tests/tests/apps.tests.tests.TestIssue37.class.php
@@ -10,8 +10,8 @@ class TestIssue37 extends TestCase {
 
    public function run()
    {
-      $this->test1();
-      //$this->test2();
+      //$this->test1();
+      $this->test2();
    }
    
    public function test1()
@@ -28,14 +28,21 @@ class TestIssue37 extends TestCase {
       $this->assert( $a->save(), 'Test guardar aaa '. print_r($a->getErrors(), true));
    }
    
+   /**
+    * test de rollback de toda la transaccion del save en cascada.
+    */
    public function test2()
    {
-      //$bot2 = Botella::get( $this->bot->getId() );
-      //$bot2 = Botella::get( $bot->getId() ); // si descomento obtengo un fatal error, deberia reportarlo en la suite...
+      $a = new AAA();
+      $c = new CCC(array(
+        'attrBBB' => 'sfasdf',
+        'attrCCC' => 'aa' // El save en cascada deberia fallar porque debe tener 5 caracteres o mas
+      ));
       
-      //$this->assert( !is_null($bot2), 'Test carga por id');
+      $a->addToBs($c);
       
-      //$this->assert( is_bool($bot2->getTapaRosca()), 'Test boolean 3');
+     
+      $this->assert( !$a->save(), 'Test rollback '. print_r($c->getErrors(), true) );
    }
    
 }
